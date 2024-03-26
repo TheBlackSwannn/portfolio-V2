7af906207dd0f1f2c038cad691705c9499028d8a
Swan Frere <swan.frere@idci-consulting.fr>
2024-03-26 18:47:38 +0100

    fix: ssr problemes

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -5,6 +5,7 @@ const app = express();
 
 const router = Router();
 router.get("/", (req, res) => res.send("Hello World!"));
+router.get("/test", (req, res) => res.send("TEEEEST!"));
 
 app.use("/", router);
 
